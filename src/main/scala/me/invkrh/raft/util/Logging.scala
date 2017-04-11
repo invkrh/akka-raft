bcf9cc967f3d1e63d48b2faf827bf3f972ba28fb
invkrh <invkrh@gmail.com>
2017-04-11 02:13:45 +0200

    Add IdentifiedLogging

--- src/main/scala/me/invkrh/raft/util/Logging.scala
+++ src/main/scala/me/invkrh/raft/util/Logging.scala
@@ -1,10 +1,17 @@
 package me.invkrh.raft.util
 
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 trait Logging {
-  private val logger = LoggerFactory.getLogger(this.getClass)
+  protected val logger: Logger = LoggerFactory.getLogger(this.getClass)
   def info(msg: String): Unit = logger.info(msg)
   def warn(msg: String): Unit = logger.debug(msg)
   def error(msg: String): Unit = logger.error(msg)
+}
+
+trait IdentifyLogging extends Logging{
+  def id: Int
+  override def info(msg: String): Unit = logger.info(s"[$id] $msg")
+  override def warn(msg: String): Unit = logger.warn(s"[$id] $msg")
+  override def error(msg: String): Unit = logger.error(s"[$id] $msg")
 }
