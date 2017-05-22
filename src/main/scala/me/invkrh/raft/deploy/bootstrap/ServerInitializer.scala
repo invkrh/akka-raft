8226c49045e63565ddcf291a919f001df3a4dc68
Hao Ren <invkrh@gmail.com>
2017-05-23 01:19:09 +0200

    Fix member index bug

--- src/main/scala/me/invkrh/raft/deploy/bootstrap/ServerInitializer.scala
+++ src/main/scala/me/invkrh/raft/deploy/bootstrap/ServerInitializer.scala
@@ -1,7 +1,5 @@
 package me.invkrh.raft.deploy.bootstrap
 
-import scala.collection.mutable.ArrayBuffer
-
 import akka.actor.{Actor, ActorRef, Props}
 import me.invkrh.raft.deploy.{AskServerID, Ready, ServerID}
 import me.invkrh.raft.server.Message.Init
@@ -11,21 +9,20 @@ object ServerInitializer {
 }
 
 class ServerInitializer(initialSize: Int) extends Actor {
-  val members = new ArrayBuffer[ActorRef]()
+  var membership: Map[Int, ActorRef] = Map()
   var remoteSysCnt = 0
   // TODO: need to deploy remotely ? as an over watcher
   override def receive: Receive = {
     case AskServerID =>
       sender ! ServerID(remoteSysCnt)
       remoteSysCnt += 1
-    case Ready(serverRef) =>
-      members.append(serverRef)
+    case Ready(id, serverRef) =>
+      membership = membership.updated(id, serverRef)
       // scalastyle:off println
-      println(s"[${members.size}/$initialSize] Find server at: ${serverRef.path}")
+      println(s"[${membership.size}/$initialSize] Find server at: ${serverRef.path}")
       // scalastyle:on println
-      if (members.size == initialSize) {
-        val membership = members.zipWithIndex.map(_.swap).toMap
-        members foreach { _ ! Init(membership) }
+      if (membership.size == initialSize) {
+        membership foreach { case (_, server) => server ! Init(membership) }
         // scalastyle:off println
         println("All servers are initialized")
         // scalastyle:on println
