50e2809eab541ba6c1b0df888b2247c6072e8795
Hao Ren <invkrh@gmail.com>
2017-07-23 11:18:28 +0200

    Update sanity check

--- src/main/scala/me/invkrh/raft/core/SanityTest.scala
+++ src/main/scala/me/invkrh/raft/core/SanityTest.scala
@@ -2,34 +2,35 @@ package me.invkrh.raft.core
 
 import akka.actor.{Actor, ActorSystem, Props}
 
-case class Holder(arr: Array[Int])
+case class Holder(coll: List[Int])
 
 // scalastyle:off println
 class AddrChecker extends Actor {
   override def receive: Receive = {
     case h: Holder =>
       println("In: " + h.toString)
-      println("In: " + h.arr.toString)
-      println("Before: " + h.arr.toList)
+      println("In: " + h.coll.toString)
+      println("Before: " + h.coll)
       Thread.sleep(5000)
-      println("After: " + h.arr.toList)
+      println("After: " + h.coll)
       context.system.terminate()
   }
 
 }
+
 object SanityTest extends App {
   val system = ActorSystem("SanityTest")
 
-  val array = Array(1, 2, 3, 4)
-  val holder = Holder(array)
+  var coll = List(1, 2, 3, 4)
+  val holder = Holder(coll)
   println("Out: " + holder.toString)
-  println("Out: " + holder.arr.toString)
-  println("Initial: " + holder.arr.toList)
+  println("Out: " + holder.coll.toString)
+  println("Initial: " + holder.coll)
 
   val checker = system.actorOf(Props(new AddrChecker))
 
   checker ! holder
   Thread.sleep(3000)
-  array(2) = 100
+  coll = List(4, 3, 2, 1)
 }
 // scalastyle:on println
