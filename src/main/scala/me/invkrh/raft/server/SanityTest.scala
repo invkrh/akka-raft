a0eb8430562f61e38986cefb3ddf9850fb533db4
Hao Ren <invkrh@gmail.com>
2017-07-23 10:41:15 +0200

    Add sanity test

---
+++ src/main/scala/me/invkrh/raft/server/SanityTest.scala
@@ -0,0 +1,33 @@
+package me.invkrh.raft.server
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class Holder(arr: Array[Int])
+
+class AddrChecker extends Actor {
+  override def receive: Receive = {
+    case h: Holder =>
+      println("In: " + h.toString)
+      println("In: " + h.arr.toString)
+      println("Before: " + h.arr.toList)
+      Thread.sleep(5000)
+      println("After: " + h.arr.toList)
+      context.system.terminate()
+  }
+
+}
+object SanityTest extends App {
+  val system = ActorSystem("SanityTest")
+
+  val array = Array(1, 2, 3, 4)
+  val holder = Holder(array)
+  println("Out: " + holder.toString)
+  println("Out: " + holder.arr.toString)
+  println("Initial: " + holder.arr.toList)
+
+  val checker = system.actorOf(Props(new AddrChecker))
+
+  checker ! holder
+  Thread.sleep(3000)
+  array(2) = 100
+}
